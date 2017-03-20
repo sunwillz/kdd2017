118091081f9522dfb8f6af885eb9da70b8d78b75
stanpcf <fpchappy@163.com>
2017-03-20 11:50:48 +0800

    add and modified some data api

--- kdd/data_preprocess/graph.py
+++ kdd/data_preprocess/graph.py
@@ -13,14 +13,16 @@ class graph(database):
 
     @staticmethod
     def show_volume_by_time(tollgate_id, direction, start_time=None, end_time=None, start_date=None, end_date=None,
-                            freq='20Min', sumed_in_one_day=True, drop_dates=None, kind_char='-', *args, **kwargs):
+                            freq='20Min', sumed_in_one_day=True, drop_dates=None, test_data=False,
+                            kind_char='-', *args, **kwargs):
         """
         参数见`graph.get_volume_by_time`
         kind_char, 点的类型, 参考plt.plot 第三个参数
         """
         volumes = graph.get_volume_by_time(tollgate_id, direction, start_time=start_time, end_time=end_time,
                                            start_date=start_date, end_date=end_date, freq=freq,
-                                           sumed_in_one_day=sumed_in_one_day, drop_dates=drop_dates)
+                                           sumed_in_one_day=sumed_in_one_day, drop_dates=drop_dates,
+                                           test_data=test_data)
         if volumes.empty:
             print("+"*50)
             print("Data from tollgate_id={0}, direction={1} is empty.".format(tollgate_id, direction))
